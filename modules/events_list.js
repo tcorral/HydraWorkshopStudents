fd146af12068be8eee0c55bb61432c8a17036519
Tomas Corral <amischol@gmail.com>
2012-10-31 02:27:11 +0100

    Fifth step - Set  EventsCallbacks

--- modules/events_list.js
+++ modules/events_list.js
@@ -8,6 +8,52 @@
 	core.register( 'events_list', function ( bus ) {
 		return {
 			oList: null,
+			oEventsCallbacks: {
+				'events:cleanList': function () {
+					this.oList.innerHTML = '';
+				},
+				'events:byDay': function ( oData ) {
+					var self = this;
+					ajax.call( {
+						url: ns.url.generateDatabaseURL,
+						dataType: 'json',
+						success: function ( oJSON ) {
+							self.fillList( ns.data.getFilteredDataByDay(oJSON, oData.day, oData.month, oData.year) );
+							bus.publish( 'schedule', 'events:cleanDetail', {} );
+						},
+						error: function ( oError ) {
+						}
+					} );
+				},
+				'events:byMonth': function ( oData ) {
+					var self = this;
+					ajax.call( {
+						url: ns.url.generateDatabaseURL,
+						dataType: 'json',
+						success: function ( oJSON ) {
+							self.fillList( ns.data.getFilteredDataByMonth(oJSON, oData.day, oData.month, oData.year) );
+							bus.publish( 'schedule', 'events:cleanDetail', {} );
+						},
+						error: function ( oError ) {
+						}
+					} );
+				}
+			},
+			fillList: function ( aData ) {
+				this.oList.innerHTML = '';
+				var aArray = aData.slice( 0 ),
+					oItem = aArray.shift(),
+					oLI;
+
+				while ( oItem ) {
+					oLI = doc.createElement( 'li' );
+					oLI.id = oItem.id;
+					oLI.innerHTML = '<div><span>Title: ' + oItem.title + '</span><br/><span>Day: ' + oItem.day + '</span></div>';
+					this.oList.appendChild( oLI );
+					oItem = aArray.shift();
+				}
+				aArray = oItem = oLI = null;
+			},
 			setBehavior: function () {
 				this.oList.addEventListener( 'click', function ( eEvent ) {
 
@@ -15,6 +61,7 @@
 			},
 			init: function () {
 				this.oList = doc.getElementById( "events_list" );
+				bus.subscribe( 'schedule', this );
 				this.setBehavior();
 			}
 		};
