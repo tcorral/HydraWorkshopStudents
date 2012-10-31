12d692fad9fe3521c9873adb0314bd200c0819d8
Tomas Corral <amischol@gmail.com>
2012-10-31 02:38:28 +0100

    Tenth step - start calendar

---
+++ libs/init.js
@@ -0,0 +1,8 @@
+/*global window, document, App, CalendarLocale_ES, Calendar, Hydra*/
+(function ( win, doc, ns, core, und ) {
+	'use strict';
+	if ( ns === und ) {
+		ns = win;
+	}
+	core.start('calendar');
+}( window, document, App, Hydra.module ));
