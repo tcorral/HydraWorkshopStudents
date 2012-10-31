f0826b683b71234662010f73997e94598eaf6048
Tomas Corral <amischol@gmail.com>
2012-10-31 02:35:31 +0100

    Eighth step - set EventsCallbacks

--- modules/event_detail.js
+++ modules/event_detail.js
@@ -7,6 +7,39 @@
 	core.register( 'event_detail', function ( bus ) {
 		return {
 			oDetail: null,
+			oEventsCallbacks: {
+				'global:sample': function( oData )
+				{
+					alert('Calling Global sample');
+				},
+				'events:loadDetailInfo': function ( oData ) {
+					var self = this;
+					ajax.call( {
+						url: ns.url.generateDatabaseURL,
+						dataType: 'json',
+						success: function ( oJSON ) {
+							self.createDetail( ns.data.getFilteredDataById( oJSON, parseInt( oData.id, 10 ) ) );
+						},
+						error: function ( oError ) {
+						}
+					} );
+				},
+				'events:cleanDetail': function ( oData ) {
+					this.oDetail.innerHTML = '';
+				}
+			},
+			createDetail: function ( oData ) {
+				var oDetail = doc.createElement( "div" ),
+					oHeader = doc.createElement( "h1" ),
+					oDescription = doc.createElement( "div" );
+
+				this.oDetail.innerHTML = '';
+				oHeader.innerHTML = oData.title;
+				oDescription.innerHTML = oData.description;
+				oDetail.appendChild( oHeader );
+				oDetail.appendChild( oDescription );
+				this.oDetail.appendChild( oDetail );
+			},
 			init: function () {
 				this.oDetail = doc.getElementById( "detail" );
 				bus.subscribe( 'schedule', this );
